599cafebc9ff3beffccef4e39377c0a11446300a
Gwen Bachour <42254686+DoughyDoe@users.noreply.github.com>
2018-10-15 18:49:37 -0700

    Added Code

---
+++ ElectroMorse/Assets/Scripts/HealthTracking.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTracking : MonoBehaviour {
+    public Sprite[] HealthStages;
+    public GameObject player;
+    public HealthManagement playerHealthManager;
+
+    private float maxHp;
+    // Use this for initialization
+    void Start () {
+        maxHp = playerHealthManager.HP;
+        if (HealthStages.Length > 0)
+            GetComponent<SpriteRenderer>().sprite = HealthStages[HealthStages.Length - 1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float HP = playerHealthManager.HP;
+        float stageCount = HealthStages.Length;
+        int healthStageIndex = 0;
+        if (HP < maxHp)
+        {
+            
+            float stage = (HP * stageCount) / maxHp;
+            healthStageIndex = (int)Mathf.Ceil(stage);
+        }
+        else
+        {
+            healthStageIndex = (int)stageCount;
+        }
+        
+        if (healthStageIndex < HealthStages.Length && healthStageIndex >= 0)
+            GetComponent<SpriteRenderer>().sprite = HealthStages[healthStageIndex];
+    }
+}
---
+++ ElectroMorse/Assets/turnOff.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnOff : MonoBehaviour {
+
+    public float exitTime = 7.75f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Invoke("TurnOff", exitTime);
+	}
+
+    void TurnOff()
+    {
+        gameObject.SetActive(false);
+    }
+}
